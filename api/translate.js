a65e0a4cfb94248edaaf112973a4f83d28382f93
begggi92 <127031891+begggi92@users.noreply.github.com>
2025-08-15 10:19:32 +0600

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -1,3 +1,12 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // preflight
+  }
+  
 import fs from "fs";
 import path from "path";
 import fetch from "node-fetch";
@@ -50,3 +59,4 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Ошибка перевода" });
   }
 }
+
